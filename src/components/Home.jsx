4958b562bd72b9957500e132cf1eaeafce123cab
Victorafx <victor.auxietrefontes@laposte.net>
2020-04-29 15:17:56 +0200

    feat(home): add home content

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+import Modal from './Modal';
+
+function Home() {
+  return (
+    <div>
+      <h1 className="homeTitle">CamAthon</h1>
+      <div className="homeText">
+        <p>Welcome! Prepare your travel before your luggages</p>
+        <p>Choose the country that you want to visit, then the city you are interested by and enjoy your journey.</p>
+      </div>
+      <Modal />
+    </div>
+  );
+}
+export default Home;
