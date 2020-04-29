3f7deda385e7bb5d9a56dc0147228a878b146ca9
jojo051 <youpi051@yahoo.fr>
2020-04-29 14:02:46 +0200

    refector(cameralive):add div in component

---
+++ src/CameraLive.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function CameraLive() { 
+  <div>
+    <div>
+      <h1> Camera title </h1>
+    </div>
+    <div>
+      retour cam
+    </div>
+    <button> camera proche </button>
+    <button> go to home </button>
+  </div>
+}
+
+export default CameraLive;
